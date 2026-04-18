2d6fbaad126746cec6d98ebf33220ea7a77c747c
vkk58 <twicvk@gmail.com>
2026-04-18 17:35:23 +0300

    after review

--- e2e/createOrder.spec.ts
+++ e2e/createOrder.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from '@playwright/test'
 
-async function openmodalWindow(page) {
-  await page.locator('[id="692889f16bf770001bfeb4d6"]').click()
+const IdConst = '692889f16bf770001bfeb4d6'
+
+async function openModalWindow(page) {
+  await page.locator(`[id="${IdConst}"]`).click()
   await page.waitForTimeout(2000)
 }
 
 async function moveItem2Order(page) {
   const firstItem = page.locator('[id="692889f16bf770001bfeb4cc"]')
-  const secondItem = page.locator('[id="692889f16bf770001bfeb4d6"]')
+  const secondItem = page.locator(`[id="${IdConst}"]`)
   const orderSpace = page.locator(
     '[class*="burger-constructor__burger_constructor"]'
   )
@@ -20,6 +22,9 @@ async function moveItem2Order(page) {
 
 test.describe('Тестирование процесса создания заказа', () => {
   test.beforeEach(async ({ page }) => {
+    await page.routeFromHAR('./e2e/hars/ingredients.har', {
+      url: '**api/ingredients',
+    })
     await page.goto('/')
   })
   test('Перетаскиваем ингредиенты в конструктор', async ({
@@ -32,14 +37,14 @@ test.describe('Тестирование процесса создания зак
   test('открытие модального окна с описанием ингредиента', async ({
     page,
   }): Promise<void> => {
-    await openmodalWindow(page)
+    await openModalWindow(page)
     await expect(page.getByText('Детали ингредиента')).toBeVisible()
   })
 
   test('отображение в модальном окне данных ингредиента', async ({
     page,
   }): Promise<void> => {
-    await page.locator('[id="692889f16bf770001bfeb4d6"]').click()
+    await page.locator(`[id="${IdConst}"]`).click()
     await page.waitForTimeout(2000)
     await expect(page.getByText('Калории,ккал')).toBeVisible()
     await expect(page.getByText('Белки, г')).toBeVisible()
@@ -50,7 +55,7 @@ test.describe('Тестирование процесса создания зак
   test('закрытие модальных окон при клике на кнопку закрытия', async ({
     page,
   }): Promise<void> => {
-    await openmodalWindow(page)
+    await openModalWindow(page)
     await page.locator('[class*="modal__closeIcon"]').click()
     await expect(page.getByText('Детали ингредиента')).toBeHidden()
   })
@@ -60,23 +65,14 @@ test.describe('Тестирование процесса создания зак
       localStorage.setItem('accessToken', 'accessTokenValue')
     })
 
-    await moveItem2Order(page)
-
-    await page.route('**/api/orders', async (route) => {
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({
-          success: true,
-          name: 'Тестовый заказ',
-          order: { number: 12345 },
-        }),
-      })
+    await page.routeFromHAR('./e2e/hars/createOrder.har', {
+      url: '**api/orders',
     })
 
+    await moveItem2Order(page)
     await page.getByText('Оформить заказ').click()
     await page.waitForTimeout(2000)
 
-    await expect(page.getByText('12345')).toBeVisible()
+    await expect(page.getByText('идентификатор заказа')).toBeVisible()
   })
 })
